1c93792fd1f62a9c1fd1172377c91491364fba44
GustavoAraujoBorrego <souza.araujo.gustavo@escola.pr.gov.br>
2025-08-20 12:12:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quem foi o capitão do penta do BRASIL em 2002?',
+    'Foi CAFU'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o jogador com mais partidas no futebol ?',
+    'É o jogador FABÍO DO FLUMINENSE'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o jogador mais caro do FLAMENGO?',
+    'É O JOGADOR SAMUEL LINO'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o jogador com mais gols no novo MARACANÃ?',
+    'O JOGADOR É ZICO'
+)
